20391812d601ad0b17df7868ff0bda7fcdcc9b91
siphonzama <sipho1728@gmail.com>
2024-04-02 14:18:18 -0700

    correction 3

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,6 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (ValueError, TypeError):
             pass
-        print()
-        Return
+    print()
+    return
+
