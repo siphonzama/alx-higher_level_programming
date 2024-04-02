7a7096db013cf49ac6c8fc4ea9376a94cb718343
siphonzama <sipho1728@gmail.com>
2024-04-02 14:28:32 -0700

    correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[i], end="")
             num += 1
-        except IndexError
+        except IndexError:
             break
-        print("")
-        Return
+    print("")
+    return
