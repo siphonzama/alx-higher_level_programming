63b758a60c1e49b4e89a9baea55948cdf1efa0d7
siphonzama <sipho1728@gmail.com>
2024-05-01 03:22:06 -0700

    correction

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
-"""Add func module"""
+"""Defines a function adds 2 integers
+Attributes:
+    add_integer: function that adds two integers
+"""
 
 
 def add_integer(a, b=98):
-    """Adds 2 int or float values, return int"""
-    if type(a) not in [int, float]:
-        raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
-        raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    """Add 2 integers or float values
+
+    Args:
+        a (int): 1st argument
+        b (int, optional): 2nd argument. Defaults to 98.
+
+    Raises:
+        TypeError: if a or b are not int or float
+
+    Returns:
+        int: a + b
+    """
+    if isinstance(a, float):
+        a = int(a)
+
+    if isinstance(b, float):
+        b = int(b)
+
+    if not isinstance(a, int):
+        raise TypeError("a must be integer")
+
+    if not isinstance(b, int):
+        raise TypeError("b must be integer")
+    return a + b
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
-"""name func module"""
+"""Defines a function that prints my name.
+
+Attributes:
+    say_my_name: function that prints my name.
+"""
 
 
 def say_my_name(first_name, last_name=""):
-    """name,
-    return new name"""
-    if type(first_name) is not str:
+    """Prints my name.
+
+    Args:
+        first_name (str): My first name.
+        last_name (str, optional): My second name. Defaults to "".
+
+    Raises:
+        TypeError: If either first name or last name is not a string.
+    """
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    print("My name is {:s} {:s}".format(first_name, last_name))
+
+    print("My name is {} {}".format(first_name, last_name))
