afbf1a258b5b2c949376eec9eead1a3c13649536
siphonzama <sipho1728@gmail.com>
2024-08-12 08:53:29 -0700

    task correction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 --
-SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month >= 7 and month <= 8 GROUP BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) as avg_temp FROM temperatures WHERE month >= 7 and month <= 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
